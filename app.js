cdad93e37713ba16c60addef1b9032fdf7e707c9
Chen <chenop@gmail.com>
2016-09-10 21:42:54 +0300

    - improve tooltip

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $("input:password").keypress(fixLanguage);
 
 function showTooltip() {
 	$(this).qtip({ // Grab some elements to apply the tooltip to
-		content: '<img src="https://cdn2.iconfinder.com/data/icons/blue-rounded-set/154/password-pin-code-pass-access-32.png"/>Coverting to english!',
+		content: '<div><img style="vertical-align:middle" src="https://cdn2.iconfinder.com/data/icons/blue-rounded-set/154/password-pin-code-pass-access-32.png"><span style="padding-left: 10px">Coverting to english!</span></div>',
 		show: true,
 		position: {
 			my: 'top center',  // Position my top left...
