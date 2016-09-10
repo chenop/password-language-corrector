07f47d1fd931a049af88bba33c48c8ad86d9c1bf
Chen <chenop@gmail.com>
2016-09-10 03:11:10 +0300

    - refactor

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ var languageConvertor = new LanguageConvertor("he_en", map);
 
 $("input").keypress(fixLanguage);
 
+function showTooltip() {
+	$(this).qtip({ // Grab some elements to apply the tooltip to
+		content: 'Coverting to english!',
+		show: true,
+		position: {
+			my: 'top center',  // Position my top left...
+			at: 'bottom center', // at the bottom right of...
+			target: $(this) // my target
+		},
+		style: {
+			classes: 'qtip-blue',
+		}
+	})
+}
+function hideTooltip() {
+	$(this).qtip("hide");
+}
+function appendKey(convertedKey) {
+	$(this).val($(this).val() + convertedKey);
+}
 function fixLanguage(event) {
 	if (languageConvertor.isExist(event.key)) {
 		event.preventDefault();
 		var convertedKey = languageConvertor.tryConvert(event.key);
-		$(this).val($(this).val() + convertedKey);
-		$(this).qtip({ // Grab some elements to apply the tooltip to
-			content:  'Coverting to english!',
-			show: true,
-			position: {
-				my: 'top center',  // Position my top left...
-				at: 'bottom center', // at the bottom right of...
-				target: $(this) // my target
-			},
-			style: {
-				classes: 'qtip-blue',
-			}
-		})
-		//console.log(convertedKey);
+
+		appendKey.call(this, convertedKey);
+		showTooltip.call(this);
 	}
-	$(this).qtip("hide");
+	hideTooltip.call(this);
 }
