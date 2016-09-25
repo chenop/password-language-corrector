2ba7c7029bdc2445838102724ddd1df789424f42
Chen <chenop@gmail.com>
2016-09-25 08:37:04 +0300

    - Update Icon

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function createLanguageConvertor() {
 	inputs.keypress(fixLanguage);
 
 	inputs.qtip({ // Grab some elements to apply the tooltip to
-		content: '<div><img style="vertical-align:middle" src="https://cdn2.iconfinder.com/data/icons/blue-rounded-set/154/password-pin-code-pass-access-32.png"><span style="padding-left: 10px">Coverting to english!</span></div>',
+		content: '<div><img style="vertical-align:middle" src="https://www.iconfinder.com/icons/650961/1071962/128/raster?token=1474780419-ZhcTAiu1Ia3eD5F6Zx3UfbYvybhyBkGc-L%2BgHdKLA84jqc/mYCQ0cNUV48Z4%3D"><span style="padding-left: 10px">Coverting to english!</span></div>',
 		show: false,
 		position: {
 			my: 'top center',  // Position my top left...
